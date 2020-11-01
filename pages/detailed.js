ef1ac32bbda30473f480a57bc94f2b699829ec5c
Tosei <359387050@qq.com>
2020-11-01 11:19:14 +0900

    code write

--- pages/detailed.js
+++ pages/detailed.js
@@ -13,44 +13,28 @@ import ReactMarkDown from 'react-markdown';
 import MarkdownNavbar from 'markdown-navbar';
 import 'markdown-navbar/dist/navbar.css';
 import axios from 'axios';
+import marked from 'marked';
+import hljs from 'highlight.js';
+import 'highlight.js/styles/monokai-sublime.css';
 
-export default function Detailed() {
-  let markdown =
-    '\n' +
-    '# P01:课程介绍和环境搭建\n\n' +
-    '[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n' +
-    '> Mditor 是一个简洁、易于集成、方便扩展、期望舒服的编写 markdown 的编辑器，仅此而已... \n\n' +
-    '**这是加粗的文字**\n\n' +
-    '*这是倾斜的文字*`\n\n' +
-    '***这是斜体加粗的文字***\n\n' +
-    '~~这是加删除线的文字~~ \n\n' +
-    '`console.log(111)` \n\n' +
-    '# p02:来个Hello World 初始Vue3.0\n' +
-    '> aaaaaaaaa\n' +
-    '>> bbbbbbbbb\n' +
-    '>>> cccccccccc\n' +
-    '***\n\n\n' +
-    '# p03:Vue3.0基础知识讲解\n' +
-    '> aaaaaaaaa\n' +
-    '>> bbbbbbbbb\n' +
-    '>>> cccccccccc\n\n' +
-    '# p04:Vue3.0基础知识讲解\n' +
-    '> aaaaaaaaa\n' +
-    '>> bbbbbbbbb\n' +
-    '>>> cccccccccc\n\n' +
-    '#5 p05:Vue3.0基础知识讲解\n' +
-    '> aaaaaaaaa\n' +
-    '>> bbbbbbbbb\n' +
-    '>>> cccccccccc\n\n' +
-    '# p06:Vue3.0基础知识讲解\n' +
-    '> aaaaaaaaa\n' +
-    '>> bbbbbbbbb\n' +
-    '>>> cccccccccc\n\n' +
-    '# p07:Vue3.0基础知识讲解\n' +
-    '> aaaaaaaaa\n' +
-    '>> bbbbbbbbb\n' +
-    '>>> cccccccccc\n\n' +
-    '``` var a=11; ```';
+export default function Detailed(props) {
+  console.log(props);
+  const renderer = new marked.Renderer();
+  marked.setOptions({
+    renderer: renderer,
+    gfm: true,
+    pedantic: false,
+    sanitize: false,
+    tables: true,
+    breaks: false,
+    smartLists: true,
+    smartypants: false,
+    highlight: function (code) {
+      return hljs.highlightAuto(code).value;
+    },
+  });
+
+  let html = marked(props.articleCointent);
 
   return (
     <>
@@ -67,27 +51,26 @@ export default function Detailed() {
                   <a href="/">首页</a>
                 </Breadcrumb.Item>
                 <Breadcrumb.Item>视频列表</Breadcrumb.Item>
-                <Breadcrumb.Item>xxx</Breadcrumb.Item>
+                <Breadcrumb.Item>{props.title}</Breadcrumb.Item>
               </Breadcrumb>
             </div>
             <div>
-              <div className="detailed-title">
-                React实战视频教程-Tosei Blog开发(更新08集)
-              </div>
+              <div className="detailed-title">{props.title}</div>
               <div className="list-icon center">
                 <span>
-                  <CalendarOutlined /> 2019-06-28
+                  <CalendarOutlined /> {props.addTime}
                 </span>
                 <span>
-                  <FolderOutlined /> 视频教程
+                  <FolderOutlined /> {props.addTime}
                 </span>
                 <span>
-                  <FireOutlined /> 5498人
+                  <FireOutlined /> {props.viewCount}人
                 </span>
               </div>
-              <div className="detailed-content">
-                <ReactMarkDown children={markdown} allowDangerousHtml={false} />
-              </div>
+              <div
+                className="detailed-content"
+                dangerouslySetInnerHTML={{ __html: html }}
+              ></div>
             </div>
           </div>
         </Col>
@@ -97,11 +80,6 @@ export default function Detailed() {
           <Affix offsetTop={10}>
             <div className="detailed-nav comm_box">
               <div className="nav-title">文章目录</div>
-              <MarkdownNavbar
-                className="article-menu"
-                source={markdown}
-                ordered={false}
-              ></MarkdownNavbar>
             </div>
           </Affix>
         </Col>
@@ -112,11 +90,9 @@ export default function Detailed() {
 }
 
 Detailed.getInitialProps = async (context) => {
-  console.log(context);
   let id = context.query.id;
   const promise = new Promise((resolve) => {
-    axios('http://127.0.0.1:7001/default/getArticleById' + id).then((res) => {
-      console.log(title);
+    axios('http://127.0.0.1:7001/default/getArticleById/' + id).then((res) => {
       resolve(res.data.data[0]);
     });
   });
